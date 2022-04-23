e99904ffbfad825d6f4ef14cdb2eeda94e7b5985
donia-haron <doniaharon.1852@gmail.com>
2022-04-23 14:35:35 +0200

    first commit

--- app/src/main/java/com/example/afinal/MainActivity.java
+++ app/src/main/java/com/example/afinal/MainActivity.java
@@ -3,64 +3,61 @@ package com.example.afinal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.animation.ObjectAnimator;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
-import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.view.animation.BounceInterpolator;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.hanks.htextview.HTextView;
 import com.hanks.htextview.HTextViewType;
 
 public class MainActivity extends AppCompatActivity {
-    ImageView logo;
+    TextView logo;
     private int mCounter = 10;
     private HTextView hTextView;
 
-    //prediction
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        logo = (ImageView) findViewById(R.id.profile_image);
+        logo = (TextView) findViewById(R.id.profile_image);
+
         //image animation
         ObjectAnimator moveAnim = ObjectAnimator.ofFloat(logo, "Y", 600);
         moveAnim.setDuration(2000);
         moveAnim.setInterpolator(new BounceInterpolator());
         moveAnim.start();
+
         //text animation
         hTextView = (HTextView) findViewById(R.id.text2);
         hTextView.setTextColor(Color.WHITE);
         hTextView.setAnimateType(HTextViewType.TYPER);
-
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
 
+                ///  checkin  registrations   2unreg
                 SharedPreferences sh = getSharedPreferences("MySharedPref", MODE_PRIVATE);
-                String id = sh.getString("id", "");
+                String id = sh.getString("sid", "");
                 Log.i("iddd", id);
-                if (id != "") {
-                    Intent i = new Intent(getApplicationContext(), SigninActivity.class);
-                    startActivity(i);
 
-                } else {
+                if (id != "") {
                     Intent i = new Intent(getApplicationContext(), HomeActivity.class);
+                    startActivity(i);
+                } else {
+                    Intent i = new Intent(getApplicationContext(), SigninActivity.class);
                     startActivity(i);
                 }
             }
         }, 5000);
-
-
     }
-
-
 }
--- app/src/main/java/com/example/afinal/ProfileActivity.java
+++ app/src/main/java/com/example/afinal/ProfileActivity.java
@@ -1,43 +1,33 @@
 package com.example.afinal;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager.widget.ViewPager;
 
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.LinearLayout;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-import com.google.android.material.tabs.TabLayout;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ProfileActivity extends AppCompatActivity {
-
-    String URL = R.string.url+"/user/";
-    String URL1 = R.string.url+"/usercar/";
+    String URL = "http://192.168.220.207:8000/api/user/";
     EditText name;
     EditText email;
     EditText phone;
@@ -45,92 +35,26 @@ public class ProfileActivity extends AppCompatActivity {
     EditText nid;
     EditText address;
     EditText dob;
-TextView back;
-    EditText carnum;
-    EditText category;
-    EditText type;
-    EditText color;
-    TabLayout tabLayout;
-    ViewPager viewPager;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-//        name = (EditText) findViewById(R.id.name);
-//        email = (EditText) findViewById(R.id.email);
-//        phone = (EditText) findViewById(R.id.phone);
-//        address = (EditText) findViewById(R.id.address);
-//        nid = (EditText) findViewById(R.id.nid);
-//        dob = (EditText) findViewById(R.id.dob);
-//        gender = (EditText) findViewById(R.id.gender);
-//
-//        carnum = (EditText) findViewById(R.id.carnum);
-//        category = (EditText) findViewById(R.id.category);
-//        type = (EditText) findViewById(R.id.type);
-//        color = (EditText) findViewById(R.id.color);
-       back = (TextView) findViewById(R.id.back);
+
+        name = (EditText) findViewById(R.id.name);
+        email = (EditText) findViewById(R.id.email);
+        phone = (EditText) findViewById(R.id.phone);
+        address = (EditText) findViewById(R.id.address);
+        nid = (EditText) findViewById(R.id.nid);
+        dob = (EditText) findViewById(R.id.dob);
+        gender = (EditText) findViewById(R.id.gender);
+        parseApiData();
 
 
-        tabLayout=(TabLayout)findViewById(R.id.tabLayout);
-        viewPager=(ViewPager)findViewById(R.id.viewPager);
-        tabLayout.addTab(tabLayout.newTab().setText("Personal Info"));
-        tabLayout.addTab(tabLayout.newTab().setText("Car Info"));
-        tabLayout.addTab(tabLayout.newTab().setText("Registrations"));
-
-        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
-
-        final MyAdapter adapter = new MyAdapter(this,getSupportFragmentManager(), tabLayout.getTabCount());
-        viewPager.setAdapter(adapter);
-
-        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
-
-        TabLayout.Tab tab = tabLayout.getTabAt(0);
-        tab.view.setAlpha((float) 0.5);
-        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                viewPager.setCurrentItem(tab.getPosition());
-                tab.view.setAlpha((float) 0.5);
-
-            }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-                tab.view.setAlpha((float) 1);
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
-            }
-        });
-
-
-        back.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
-                startActivity(intent);
-            }
-        });
-
-
-
-
-
-
-
-
-//
-//    parseApiData();
-//        parseApicar();
     }
 
-
     public void parseApiData() {
-        SharedPreferences sh = getSharedPreferences("MySharedPref", MODE_PRIVATE);
-        String id = sh.getString("id", "");
+        SharedPreferences sh = this.getSharedPreferences("MySharedPref", Context.MODE_PRIVATE);
+        String id = sh.getString("sid", "");
         Log.i("iddd", id);
 
         StringRequest s = new StringRequest(Request.Method.GET, URL + id, new Response.Listener<String>() {
@@ -177,48 +101,76 @@ TextView back;
 
     }
 
-    public void parseApicar() {
-        SharedPreferences sh = getSharedPreferences("MySharedPref", MODE_PRIVATE);
-        String id = sh.getString("id", "");
+    public void logout(View view) {
+        SharedPreferences preferences = getSharedPreferences("MySharedPref", Context.MODE_PRIVATE);
+        preferences.edit().remove("sid").commit();
+        Intent i = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(i);
+
+    }
+
+    public void update(View view) {
+
+        String url = "http://192.168.220.207:8000/api/user/update/";
+        SharedPreferences sh = this.getSharedPreferences("MySharedPref", Context.MODE_PRIVATE);
+        String id = sh.getString("sid", "");
         Log.i("iddd", id);
 
-        StringRequest s = new StringRequest(Request.Method.GET, URL1 + id, new Response.Listener<String>() {
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        StringRequest request = new StringRequest(Request.Method.POST, url+id, new com.android.volley.Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
-                Log.i("result", response);
+
                 try {
-                    JSONArray x = new JSONArray(response);
-
-                    if (x.length() > 0) {
-                        for (int i = 0; i < x.length(); i++) {
-                            JSONObject car = x.getJSONObject(i);
-                            String carnum1 = car.getString("id");
-                            carnum.setText(carnum1);
-                            String category1 = car.getString("category");
-                            category.setText(category1);
-                            String type1 = car.getString("category");
-                            type.setText(type1);
-                            String color1 = car.getString("color");
-                            color.setText(color1);
-
-
+                    JSONObject respObj = new JSONObject(response);
+                    Log.i("response", response);
+                    Handler handler = new Handler();
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            Intent i = new Intent(getApplicationContext(), HomeActivity.class);
+                            startActivity(i);
                         }
+                    }, 2000);
 
-
-                    }
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
             }
-        }, new Response.ErrorListener() {
+        }, new com.android.volley.Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                Log.i("nelaa", String.valueOf(error));
-
+                Log.i("eroororr", String.valueOf(error));
             }
-        });
-        RequestQueue r = Volley.newRequestQueue(this);
-        r.add(s);
+        }) {
+            @Override
+            protected Map getParams() {
+
+                Map<String, Object> params = new HashMap<>();
+
+
+                params.put("username",name.getText().toString() );
+                params.put("address", address.getText().toString());
+                params.put("email", email.getText().toString());
+                params.put("gender", gender.getText().toString());
+                params.put("phone", phone.getText().toString());
+                params.put("role", "securityman");
+                params.put("dob", dob.getText().toString());
+
+
+                Log.i("param", String.valueOf(params));
+
+                return params;
+            }
+        };
+
+        queue.add(request);
+
+
+
+
+
 
     }
 
